ab54f03fdaa995963862cb180fbc5b9a4c90f278
Jorge Henrique Piazentin Ono <jorgehpo@nyu.edu>
2021-03-08 20:45:13 -0500

    adding support for mongodb in videoexplorer

--- VideoExplorer/camera_view.py
+++ VideoExplorer/camera_view.py
@@ -3,28 +3,57 @@ import numpy as np
 import pandas as pd
 from pathlib import Path
 from matplotlib import pyplot
+import pymongo
+import json
+
+
 ##############
 # Setting up data and paths
-img_path = Path("/Volumes/GoogleDrive/My Drive/ColabData/video_images")
-video_df = pd.read_csv("camera_view.csv")
-camera_views = video_df["true_camera_view"].unique()
+with open("config.json", "r") as f:
+    config = json.load(f)
+    conn_path = config["mongodb_url"]
+    img_path = Path(config["img_path"])
 
-def divide_chunks(l, n):      
-    # divide list l in chunks of size n
-    for i in range(0, len(l), n):  
-        yield l[i:i + n] 
+##############
+# MongoDB connector
+try:
+    client = pymongo.MongoClient(conn_path)
+    db = client.ltpt
+    camera_views = db["videos"].distinct("predicted_view")
+except Exception as e:
+    st.write("# MongoDB Connection Error");
+    st.write(str(e))
 
-def plot_image_grid(df, n_columns):
-    n = df.shape[0]
-    for idxs_chunk in divide_chunks(range(n), n_columns):
-        cols = st.beta_columns(3)
-        for col_idx, img_idx in enumerate(idxs_chunk):
-            path = img_path / df["file"].iloc[img_idx]
-            pred_class = df["pred_camera_view"].iloc[img_idx]
-            img = pyplot.imread(path)
-            cols[col_idx].write("**Pred**: "+ pred_class)
-            cols[col_idx].image(img, width=200)
+##############
+# Mappings
+map_letter_view = {"A": "Behind Pitcher", "B": "Batter", "C": "Pitcher", "D": "Behind Home"}
+map_view_letter = {"Behind Pitcher": "A", "Batter": "B", "Pitcher": "C", "Behind Home": "D"}
 
+def query_db(true_view, correct):
+    true_letter = map_view_letter[true_view]
+    if correct:
+        return db["videos"].find({"predicted_view": true_view, "view": true_letter})
+    else:
+        return db["videos"].find({"predicted_view": {"$ne": true_view}, "view": true_letter})
+
+##############
+# Image Grid
+
+def plot_image_grid(query, n_columns):
+    col_idx = 0
+    cols = st.beta_columns(n_columns)
+    for document in query:
+        pred_class = document["predicted_view"]
+        true_class = map_letter_view[document["view"]]
+        image_name = str(document["_id"]) + "_" + document["view"] + ".png"
+        image_path = img_path / image_name
+        img = pyplot.imread(image_path)
+        cols[col_idx].write("**Pred**: "+ pred_class)
+        cols[col_idx].image(img, width=200)
+        col_idx+=1
+        if col_idx == n_columns:
+            col_idx = 0
+            cols = st.beta_columns(n_columns)
 
 ##############
 # Streamlit Dashboard
@@ -35,13 +64,9 @@ def main():
     correct_pred_option = st.radio(
         "Show Predictions",
         ("Correct", "Incorrect"))
-    filtered_df = video_df[video_df["true_camera_view"] == true_camera_view]
-    if correct_pred_option == "Correct":
-        filtered_df = filtered_df[filtered_df["true_camera_view"] == filtered_df["pred_camera_view"]]
-    else:
-        filtered_df = filtered_df[filtered_df["true_camera_view"] != filtered_df["pred_camera_view"]]
-    filtered_df = filtered_df.iloc[:3*10]
-    plot_image_grid(filtered_df, 3)
+    correct =  correct_pred_option == "Correct"
+    query = query_db(true_view=true_camera_view, correct=correct).limit(3*5) # 5 rows
+    plot_image_grid(query, 3)
 
 
 ##############
