0233a9dd41c629109376d16f6ee3e4f568d01a82
ypiadyk <yurii.piadyk@gmail.com>
2021-03-22 14:51:19 -0400

    Include masks support

--- calibration/segmentator.py
+++ calibration/segmentator.py
@@ -2,6 +2,7 @@ import os
 import sys
 import glob
 import time
+import joblib
 import functools
 import numpy as np
 from matplotlib import pyplot
@@ -18,6 +19,23 @@ from PyQt5 import uic, QtCore, QtWidgets
 MOUSE_LEFT = 1
 MOUSE_MIDDLE = 2
 MOUSE_RIGHT = 3
+
+
+def load_image(filename):
+    img = pyplot.imread(filename)[:, :, :3]
+    return (255 * img).astype(np.uint8) if img.dtype == np.dtype(np.float32) else np.copy(img)
+
+
+def apply_mask(img, mask):
+    if mask is not None:
+        r, c = np.nonzero(mask)
+        img[r, c, :] = [255, 255, 0]
+    return img
+
+
+def ensure_exists(path):
+    if not os.path.exists(path):
+        os.makedirs(path, exist_ok=True)
 
 
 class QFigure(QtWidgets.QWidget):
@@ -67,7 +85,7 @@ class GUI():
         self.ui.resize(1600, 900)
         self.ctrl, self.shift, self.alt = False, False, False
         self.img_view, self.colors_view = None, None
-        self.sz, self.mode = 100, ""
+        self.sz, self.mode, self.button = 100, "", None
 
         self.fimg = QFigure('img')
         self.fcolors = QFigure('colors', toolbar=False)
@@ -75,10 +93,14 @@ class GUI():
         self.flayout.addWidget(self.fimg, 19)
         self.flayout.addWidget(self.fcolors, 1)
         self.ui.layout.addLayout(self.flayout, 9)
-        self.fimg.canvas.mpl_connect('button_press_event', self.on_image_click)
+        self.fimg.canvas.mpl_connect('button_press_event', self.on_image_press)
+        self.fimg.canvas.mpl_connect('button_release_event', self.on_image_release)
+        self.fimg.canvas.mpl_connect('motion_notify_event', self.on_image_move)
         self.fcolors.canvas.mpl_connect('button_press_event', self.on_color_click)
+        self.ui.filenameCB.view().setVerticalScrollBarPolicy(QtCore.Qt.ScrollBarAsNeeded)
 
         self.choose = functools.partial(self.wrap, self.on_choose_folder, "Choose folder")
+        self.locate = functools.partial(self.wrap, self.on_locate_masks, "Locate masks")
         self.search = functools.partial(self.wrap, self.on_search_files, "Search files")
         self.select = functools.partial(self.wrap, self.on_select_file, "Select file")
         self.quantize = functools.partial(self.wrap, self.on_quantize, "Quantize")
@@ -86,20 +108,24 @@ class GUI():
         self.save = functools.partial(self.wrap, self.on_save, "Save")
 
         self.ui.actionChooseFolder.triggered.connect(self.choose)
+        self.ui.actionLocateMasks.triggered.connect(self.locate)
         self.ui.actionSearchFiles.triggered.connect(self.search)
         self.ui.actionQuantize.triggered.connect(self.quantize)
         self.ui.actionAssign.triggered.connect(self.assign)
         self.ui.actionSave.triggered.connect(self.save)
 
         self.ui.choosePB.clicked.connect(self.choose)
+        self.ui.locatePB.clicked.connect(self.locate)
         self.ui.searchPB.clicked.connect(self.search)
         self.ui.quantizePB.clicked.connect(self.quantize)
         self.ui.assignPB.clicked.connect(self.assign)
         self.ui.savePB.clicked.connect(self.save)
 
         self.ui.folderLE.returnPressed.connect(self.search)
+        self.ui.masksLE.returnPressed.connect(self.search)
         self.ui.templateLE.returnPressed.connect(self.search)
         self.ui.filenameCB.currentTextChanged.connect(self.select)
+        self.ui.applyMaskCB.stateChanged.connect(self.select)
         self.ui.numColorsSP.valueChanged.connect(self.quantize)
         self.ui.suffixLE.returnPressed.connect(self.save)
 
@@ -156,13 +182,24 @@ class GUI():
         else:
             raise ValueError("Invalid directory: \'%s\'" % new_dir)
 
+    def on_locate_masks(self):
+        self.mode = ""
+        new_dir = QtWidgets.QFileDialog.getExistingDirectory(self.ui, 'Locate masks', self.ui.masksLE.text())
+
+        if os.path.isdir(new_dir):
+            self.ui.masksLE.setText(new_dir)
+
+            return "New location \'%s\'" % new_dir
+        else:
+            raise ValueError("Invalid location: \'%s\'" % new_dir)
+
     def on_search_files(self):
         self.mode = ""
         self.folder = self.ui.folderLE.text()
         self.template = self.ui.templateLE.text()
 
         self.ui.filenameCB.clear()
-        filenames = glob.glob(self.folder + "/" + self.template)
+        filenames = sorted(glob.glob(self.folder + "/" + self.template))
 
         print(len(filenames), "files:", filenames)
 
@@ -177,13 +214,33 @@ class GUI():
     def on_select_file(self):
         self.mode = ""
         self.filename = self.ui.filenameCB.currentText()
-        print(self.filename)
+        print("Image:", self.filename)
 
         path = self.folder + "/" + self.filename
         if self.filename == "" or not os.path.exists(path):
             return "No file selected"
 
-        self.img = pyplot.imread(path)
+        candidates = [self.folder + "/" + self.filename[:-4] + "_mask.png",
+                      self.folder + "/" + self.filename[:-4] + "_mask.jpg",
+                      self.ui.masksLE.text() + "/" + self.filename[:-4] + ".png",
+                      self.ui.masksLE.text() + "/" + self.filename[:-4] + ".jpg"]
+
+        candidates.extend([c[:-4] + "_" + c[-4:] for c in candidates])
+
+        mask_name, self.mask = "", None
+        for candidate in candidates:
+            if os.path.exists(candidate):
+                mask_name = candidate
+                break
+
+        if os.path.exists(mask_name):
+            self.mask = np.sum(load_image(mask_name), axis=2) > 0
+            print("Mask:", mask_name)
+
+        self.img = load_image(path)
+
+        if self.ui.applyMaskCB.isChecked():
+            self.img = apply_mask(self.img, self.mask)
 
         if self.ui.autoQuantizeCB.isChecked():
             self.quantize()
@@ -196,7 +253,7 @@ class GUI():
             self.fimg.fig.tight_layout()
             self.fimg.redraw()
 
-        return self.filename + " selected"
+        return self.filename + " selected" + (" (with mask)" if self.mask is not None else "")
 
     def map(self, labels, colors):
         img = np.zeros_like(self.img)
@@ -258,6 +315,9 @@ class GUI():
             elif self.ctrl:
                 self.mapped_colors[i, :] = [0, 255, 0]
                 self.mapping[i] = 2
+            elif self.shift:
+                self.mapped_colors[i, :] = [255, 255, 0]
+                self.mapping[i] = 6
 
         if button == MOUSE_MIDDLE:
             if self.ctrl:
@@ -266,6 +326,9 @@ class GUI():
             elif self.alt:
                 self.mapped_colors[i, :] = [255, 0, 255]
                 self.mapping[i] = 5
+            elif self.shift:
+                self.mapped_colors[i, :] = [0, 255, 255]
+                self.mapping[i] = 7
 
         if button == MOUSE_RIGHT:
             if self.alt:
@@ -274,12 +337,27 @@ class GUI():
             elif self.ctrl:
                 self.mapped_colors[i, :] = [255, 0, 0]
                 self.mapping[i] = 1
+            elif self.shift:
+                self.mapped_colors[i, :] = [0, 0, 0]
+                self.mapping[i] = 0
+
+    def on_image_press(self, e):
+        self.button = e.button
+        self.on_image_click(e)
+
+    def on_image_release(self, e):
+        self.button = None
+        self.on_image_click(e)
+
+    def on_image_move(self, e):
+        e.button = self.button
+        self.on_image_click(e)
 
     def on_image_click(self, e):
         self.update_modifiers()
-        print("Image:", e.xdata, e.ydata, e.button, self.ctrl, self.shift, self.alt)
+        # print("Image:", e.xdata, e.ydata, e.button, self.ctrl, self.shift, self.alt)
 
-        if e.xdata and e.ydata and (self.ctrl or self.alt) and self.mode != "":
+        if e.xdata and e.ydata and (self.ctrl or self.shift or self.alt) and self.mode != "":
             r, c = int(e.ydata + 0.5), int(e.xdata + 0.5)
 
             if self.mode == "mapping":
@@ -289,31 +367,33 @@ class GUI():
                 i = self.labels.reshape(self.img.shape[:2])[r, c]
                 color = self.colors[i, :]
 
-            print(i, color, self.colors[i, :])
+            # print(i, color, self.colors[i, :])
 
             self.map_color(i, e.button)
-            self.plot_mapped(from_scratch=False)
+
+            if self.button is None:
+                self.plot_mapped(from_scratch=False)
 
     def on_color_click(self, e):
         self.update_modifiers()
-        print("Color:", e.xdata, e.ydata, e.button, self.ctrl, self.shift, self.alt)
+        # print("Color:", e.xdata, e.ydata, e.button, self.ctrl, self.shift, self.alt)
 
-        if e.xdata and e.ydata and (self.ctrl or self.alt) and self.mode == "mapping":
+        if e.xdata and e.ydata and (self.ctrl or self.shift or self.alt) and self.mode == "mapping":
             i = int(e.xdata + 0.5) // self.sz
             color = self.colors[i, :]
-            print(i, color)
+            # print(i, color)
 
             self.map_color(i, e.button)
             self.plot_mapped(from_scratch=False)
 
     def to_gray(self, img):
         img[np.nonzero(img)] = 1
-        img *= np.array([1, 2, 3], dtype=np.uint8)[None, None, :]
+        img *= np.array([1, 2, 4], dtype=np.uint8)[None, None, :]
 
         gray = np.zeros(img.shape[:2], dtype=np.uint8)
         gray[...] = np.sum(img, axis=2, dtype=np.uint8)
-        gray[np.equal(gray, 4)] = 5
-        gray[np.equal(gray, 6)] = 4
+        correspondence = np.array([0, 1, 2, 6, 3, 5, 7, 4], dtype=np.uint8)
+        gray[...] = correspondence[gray.ravel()].reshape(img.shape[:2])
 
         return gray
 
@@ -324,7 +404,9 @@ class GUI():
                            [0, 255, 0],
                            [0, 0, 255],
                            [255, 255, 255],
-                           [255, 0, 255]], dtype=np.uint8)
+                           [255, 0, 255],
+                           [255, 255, 0],
+                           [0, 255, 255]], dtype=np.uint8)
 
         img.reshape((-1, 3))[...] = colors[gray.ravel(), :]
 
@@ -337,13 +419,14 @@ class GUI():
         for i in range(self.n):
             if self.mapping[i] == 0:
                 self.mapped_colors[i, :] = [0, 0, 0]
-            elif self.mapping[i] == 3:
-                self.mapped_colors[i, :] = [255, 255, 255]
-            elif self.mapping[i] == 4:
-                self.mapped_colors[i, :] = [0, 0, 0]
+            # elif self.mapping[i] == 3:
+            #     self.mapped_colors[i, :] = [255, 255, 255]
+            # elif self.mapping[i] == 7:
+            #     self.mapped_colors[i, :] = [255, 255, 255]
+            # elif self.mapping[i] == 4:
+            #     self.mapped_colors[i, :] = [0, 0, 0]
 
-        self.assigned = self.map(self.labels, self.mapped_colors)
-        self.gray = self.to_gray(np.copy(self.assigned))
+        self.gray = self.to_gray(self.map(self.labels, self.mapped_colors))
 
         if self.ui.majorityVoteCB.isChecked():
             # for i in range(2 if self.ui.twiceCB.isChecked() else 1):
@@ -359,7 +442,10 @@ class GUI():
 
             # self.plot_img(self.fimg, self.gray)
 
-        new_labels, m = measure.label(self.gray, return_num=True)
+        self.assigned = self.from_gray(self.gray)
+
+        # Disable background feature with non-achievable gray value 8
+        new_labels, m = measure.label(self.gray, background=8, return_num=True)
         self.labels, self.n, self.sz = new_labels.ravel(), m + 1, 1
         self.colors = np.zeros((self.n, 3), dtype=np.uint8)
 
@@ -404,13 +490,79 @@ class GUI():
             # self.plot_img(self.fimg, self.gray)
             self.mapped = self.from_gray(gray)
 
+        # self.mapped = apply_mask(self.mapped, self.mask)
         self.plot_img(self.fimg, self.mapped)
 
         pyplot.imsave(new_filename, self.mapped)
         return "Saved " + new_filename
 
 
+def select_single(img_in, img_out, mask_in, mask_out, has_mask=False):
+    pyplot.imsave(img_out, load_image(img_in))
+
+    if has_mask:
+        pyplot.imsave(mask_out, load_image(mask_in))
+
+
+def select_all(mask_mandatory=True):
+    all_images_path = "../all_images/"
+    all_masks_path = "../all_masks/"
+    selected_images_path = "../selected_images/"
+    selected_masks_path = "../selected_masks/"
+
+    img_names = glob.glob(all_images_path + "*.jpg")
+    mask_names = glob.glob(all_masks_path + "*.jpg")
+    img_names.extend(glob.glob(all_images_path + "*.png"))
+    mask_names.extend(glob.glob(all_masks_path + "*.png"))
+    img_names, mask_names = sorted(img_names), sorted(mask_names)
+
+    jobs, j, j_tot = [], 0, 0
+    for i, img_name in enumerate(img_names):
+        # if j_tot > 150:
+        #     break
+
+        j = j_tot // 100
+        ensure_exists(selected_images_path + str(j))
+        ensure_exists(selected_masks_path + str(j))
+
+        base_name = os.path.basename(img_name)
+        candidates = [all_masks_path + base_name[:-3] + ext for ext in ["png", "jpg"]]
+        candidates.extend([c[:-4] + "_" + c[-4:] for c in candidates])
+
+        has_mask, mask_name = False, ""
+        for candidate in candidates:
+            if os.path.exists(candidate):
+                has_mask, mask_name = True, candidate
+                break
+
+        if mask_mandatory and not has_mask:
+            print(i, base_name, "skipped in", j)
+            continue
+        else:
+            print(i, base_name, "copying in", j)
+
+        jobs.append(joblib.delayed(select_single, check_pickle=False)
+                    (img_name, selected_images_path + str(j) + "/" + base_name[:-4] + ".png",
+                     mask_name, selected_masks_path + str(j) + "/" + base_name[:-4] + ".png",
+                     has_mask=has_mask))
+
+        # img = load_image(img_name)
+        # mask = load_image(mask_name) if has_mask else None
+        #
+        # pyplot.imsave(selected_images_path + str(j) + "/" + base_name[:-4] + ".png", img)
+        # if mask is not None:
+        #     pyplot.imsave(selected_masks_path + str(j) + "/" + base_name[:-4] + ".png", mask)
+
+        j_tot += 1
+
+    print(len(jobs), "jobs")
+    joblib.Parallel(verbose=15, n_jobs=-1, batch_size=1, pre_dispatch="all")(jobs)
+
+
 if __name__ == '__main__':
+    # select_all(mask_mandatory=True)
+    # exit()
+
     app = QtWidgets.QApplication(sys.argv)
 
     gui = GUI()
