74ee4e6cea2dee614bfdd8ed7959f9d6b485220d
ypiadyk <yurii.piadyk@gmail.com>
2021-03-24 00:08:08 -0400

    Draft of working differentiable renderer

--- calibration/optimizer.py
+++ calibration/optimizer.py
@@ -1,354 +1,202 @@
-import time
-import cv2
-import numpy as np
-import matplotlib.pyplot as plt
-from scipy.spatial.transform import Rotation
-from scipy.optimize import curve_fit
-import tensorflow as tf
-from tensorflow_graphics.rendering.camera import perspective
-from tensorflow_graphics.geometry.transformation import quaternion, rotation_matrix_3d
-from tensorflow_graphics.math.optimizer import levenberg_marquardt
-
-# tf.enable_eager_execution()
+from old_optimizer import *
+# tf.debugging.set_log_device_placement(True)
+# from tensorflow_graphics.image.transformer import sample
+from tensorflow_addons import image as tfa_image
 
 
-def line(ax, p1, p2, *args, **kwargs):
-    ax.plot([p1[0], p2[0]], [p1[1], p2[1]], [p1[2], p2[2]], *args, **kwargs)
+def prep_rays(height=1080, width=1920, mask=None, plot=True):
+    aspect = width / height
+    x, z = np.meshgrid(np.linspace(-aspect, aspect, width), np.linspace(1, -1, height))
+    rays = np.stack([x, np.ones((height, width)), z], axis=2).reshape(-1, 3)
 
-
-def basis(ax, T, R, *args, length=1, **kwargs):
-    line(ax, T, T + length * R[0, :], "r")
-    line(ax, T, T + length * R[1, :], "g")
-    line(ax, T, T + length * R[2, :], "b")
-
-
-def field(ax, dim_x, dim_y, length=30, *args, label="field", **kwargs):
-    o, ex, ey = np.zeros(3), np.array([1, 0, 0]), np.array([0, 1, 0])
-
-    line(ax, o, o + ex * dim_x, "orange", linestyle="--", label=label)
-    line(ax, o, o + ey * dim_y, "orange", linestyle="--")
-    line(ax, o + ex * dim_x, o + ex * dim_x + o + ey * dim_y, "orange", linestyle="--")
-    line(ax, o + ey * dim_y, o + ex * dim_x + o + ey * dim_y, "orange", linestyle="--")
-
-    basis(ax, np.ones(3), np.eye(3), length=length)
-
-
-def axis_equal_3d(ax):
-    extents = np.array([getattr(ax, 'get_{}lim'.format(dim))() for dim in 'xyz'])
-    sz = extents[:,1] - extents[:,0]
-    centers = np.mean(extents, axis=1)
-    maxsize = max(abs(sz))
-    r = maxsize/2
-    for ctr, dim in zip(centers, 'xyz'):
-        getattr(ax, 'set_{}lim'.format(dim))(ctr - r, ctr + r)
-
-
-def cache_reference(filename, plot=False):
-    ref = cv2.imread(filename + ".png")[:, :, ::-1]
-    print(ref.shape, ref.dtype)
+    if mask is not None:
+        r_idx = np.nonzero(mask.ravel())[0]
+        rays = rays[r_idx, :]
+    else:
+        r_idx = np.arange(width * height)
 
     if plot:
-        plt.figure("Color Reference")
-        plt.imshow(ref[::10, ::10, :])
+        plt.figure("Rays", (12, 12))
+        ax = plt.subplot(111, projection='3d', proj_type='ortho')
+        ax.set_title("Rays")
 
-    ref[np.nonzero(ref)] = 1
-    ref *= np.array([1, 2, 3], dtype=np.uint8)[None, None, :]
+        p = rays[np.random.randint(0, rays.shape[0], 1000), :]
+        ax.scatter(p[:, 0], p[:, 1], p[:, 2], c="g", s=5, label="rays")
+        basis(ax, np.zeros(3), np.eye(3), length=1)
 
-    gray = np.zeros(ref.shape[:2], dtype=np.uint8)
-    gray[...] = np.sum(ref, axis=2, dtype=np.uint8)
-    gray[np.equal(gray, 6)] = 4
+        ax.set_xlabel("x")
+        ax.set_ylabel("y")
+        ax.set_zlabel("z")
+        plt.legend()
+        plt.tight_layout()
+        axis_equal_3d(ax)
 
-    np.save(filename + ".npy", gray)
-
-
-def load_reference(filename, plot=False):
-    ref = np.load(reference_filename + ".npy")
-    print(ref.shape, ref.dtype)
-
-    if plot:
-        plt.figure("Gray Reference")
-        plt.imshow(ref[::10, ::10])
-        plt.colorbar()
-
-    return ref
+    return rays, r_idx
 
 
-height, width = 1080*2, 1920*2
-
-def tf_render(ref, *vars, im_width=1920*2, im_height=1080*2, ref_dim=(430, 430), ref_origin=(355, 75), plot=False):
-    if len(vars) != 6:  # tensorflow passed vars wrapped into extra tuple
-        vars = vars[0]
-
-    print(vars)
-
+def trace_rays(tf_rays, vars, ref, ref_dim=(430, 430), ref_origin=(355, 75)):
     fov, pos_x, pos_y, pos_z, phi, theta = vars
-    assert(pos_z > 0)  # camera has to be above the ground
-    t0 = time.time()
+    assert (pos_z > 0)  # camera has to be above the ground
+    # print(ref.shape, ref)
 
-    width, height, to_rad = tf.constant(im_width), tf.constant(im_height), tf.constant(np.pi / 180.)
-    ref_origin, ref_dim = tf.constant(ref_origin, dtype=tf.float32), tf.constant(ref_dim, dtype=tf.float32)
-
-    # fov, phi, theta = tf.Variable([fov], dtype=tf.float32),\
-    #                   tf.Variable([phi], dtype=tf.float32),\
-    #                   tf.Variable([theta], dtype=tf.float32)
-    #
-    # pos_x, pos_y, pos_z = tf.Variable([pos_x], dtype=tf.float32),\
-    #                       tf.Variable([pos_y], dtype=tf.float32),\
-    #                       tf.Variable([pos_z], dtype=tf.float32)
-
-    # print(width, height, to_rad)
-    # print(fov, phi, theta)
-    # print(pos_x, pos_y, pos_z)
-
-    trans = tf.concat([pos_x, pos_y, pos_z], axis=0)
+    to_rad = tf.constant(np.pi / 180.)
+    ref_origin = tf.constant(ref_origin, dtype=tf.float32)
+    ref_dim = tf.constant(ref_dim, dtype=tf.float32)
+    # trans = tf.concat([pos_x, pos_y, pos_z], axis=0)
     rot_z = rotation_matrix_3d.from_axis_angle(tf.constant([0, 0, 1.]), phi * to_rad)
     rot_x = rotation_matrix_3d.from_axis_angle(tf.constant([1., 0, 0]), theta * to_rad)
     rot = tf.matmul(rot_x, rot_z)
-    # print(trans, "\n", rot)
-
-    # T = np.array([pos_x, pos_y, pos_z])
-    # R = Rotation.from_euler('zx', [phi, theta], degrees=True).as_matrix()
-    # print(T, "\n", R)
 
     tan_z = tf.tan(fov * to_rad / 2.)
-    tan_x = tan_z * tf.cast(width, tf.float32) / tf.cast(height, tf.float32)
-    x, z = tf.meshgrid(tf.linspace(-tan_x, tan_x, width), tf.linspace(tan_z, -tan_z, height))
-    ray = tf.stack([x, tf.ones((height, width)), z], axis=2)
-    ray = tf.reshape(ray, [-1, 3])
-    ray = tf.matmul(ray, rot)
-    # print(tan_z, tan_x, "\n")
-    # print(ray)
+    sc = tf.concat([tan_z, tf.constant([1.]), tan_z], axis=0)
+    # print(sc)
+    # tf_rays = tf.convert_to_tensor(value=np_rays, dtype=tf.float32)
+    tf_rays = tf.multiply(tf_rays, sc[None, :])
+    tf_rays = tf.matmul(tf_rays, rot)
 
-    idx_r = tf.where(ray[:, 2] < 0)[:, 0]  # only rays pointing downwards can intersect with ground plane
-    # print("idx_r:", idx_r)
-    # print(idx_r)
+    # r = tf.reduce_sum(tf_rays, axis=1)
+    # print(r)
+    # return r
 
-    dist = tf.divide(pos_z, -tf.gather(ray[:, 2], idx_r))
-    # print(dist)
-    xy = tf.gather(ray[:, :2], idx_r[:, None])[:, 0, :]
-    # print(xy)
+    dist = tf.divide(pos_z, -tf_rays[:, 2])
+    xy = tf.multiply(tf_rays[:, :2], dist[:, None])
+    xy = tf.add(xy, tf.concat([pos_x, pos_y], axis=0)[None, :])
 
-    g_xy = tf.multiply(xy,  dist[:, None])
-    g_xy = tf.add(g_xy, tf.concat([pos_x, pos_y], axis=0)[None, :])
-    # print("g_xy:", g_xy)
-    # g_xy = ray[idx_r, :2] * pos_z / (-ray[idx_r, 2][:, None]) + tf.concat([pos_x, pos_y], axis=0)
+    # r = tf.reduce_sum(xy, axis=1)
+    # print(r)
+    # return r
 
-    # ref = tf.convert_to_tensor(value=ref)
-    # print(ref.shape)
+    ri = tf.divide(tf.multiply(ref.shape[0], tf.subtract(ref_origin[0], xy[:, 1])), ref_dim[0])
+    ci = tf.divide(tf.multiply(ref.shape[1], tf.add(ref_origin[1], xy[:, 0])), ref_dim[1])
 
-    ri = tf.divide(tf.multiply(ref.shape[0], tf.subtract(ref_origin[0], g_xy[:, 1])), ref_dim[0])
-    ci = tf.divide(tf.multiply(ref.shape[1], tf.add(ref_origin[1], g_xy[:, 0])), ref_dim[1])
-    # print(ri, ci)
-    # ri = ref.shape[0] * (ref_origin[0] - g_xy[:, 1]) / ref_dim[0]
-    # ci = ref.shape[1] * (ref_origin[1] + g_xy[:, 0]) / ref_dim[1]
+    ri = tf.maximum(0, tf.minimum(ri, ref.shape[0]-1))
+    ci = tf.maximum(0, tf.minimum(ci, ref.shape[1]-1))
 
-    idx_rc = tf.where((ri >= 0) & (ri < ref.shape[0]) & (ci >= 0) & (ci < ref.shape[1]))[:, 0]
-    # print("idx_rc:", idx_rc)
+    cri = tf.stack([ci, ri], axis=1)
+    v = tfa_image.resampler(ref[None, :, :, None], cri[None, :, :])
+    # # ri = tf.floor(ri)
+    # # print(v)
+    return v[0, :, 0]
 
-    ri = tf.cast(tf.gather(ri, idx_rc), tf.int32)
-    ci = tf.cast(tf.gather(ci, idx_rc), tf.int32)
+    ri = tf.cast(tf.maximum(0, tf.minimum(ri, ref.shape[0]-1)), tf.int32)
+    ci = tf.cast(tf.maximum(0, tf.minimum(ci, ref.shape[1]-1)), tf.int32)
 
     rci = tf.stack([ri, ci], axis=1)
-    # print("rci:", rci)
-    v = tf.gather_nd(ref, rci)
-    # print("v:", v)
-    v2 = tf.scatter_nd(idx_rc[:, None], v, [g_xy.shape[0]])
-    # print("v2:", v2)
-    v3 = tf.scatter_nd(idx_r[:, None], v2, [ray.shape[0]])
-    # print("v3:", v3)
+    return tf.gather_nd(ref, rci)
 
-    img = v3
-    # img = tf.zeros((height * width), dtype=tf.uint8)
-    # # img[idx_r] = v
-    # img = tf.reshape(img, [height, width])
+    # return tf.cast(ri, tf.float32)
 
-    print("Rendering time:", time.time()-t0, "sec")
+
+def opt_render(ref, *calib, height=1080, width=1920, mask=None, plot=True, title="Unknown"):
+    vars = build_vars(calib)
+
+    np_rays, r_idx = prep_rays(height, width, mask, plot=False)
+    tf_rays = tf.convert_to_tensor(value=np_rays, dtype=tf.float32)
+
+    values = trace_rays(tf_rays, vars, ref)
 
     if plot:
-        plt.figure("Segmentation Internals", (12, 12))
-        ax = plt.subplot(111, projection='3d', proj_type='ortho')
-        ax.set_title("Segmentation Internals")
+        img = np.zeros(height*width)
+        img[r_idx] = values.numpy()
+        img = img.reshape((height, width))
 
-        ray, g_xy = ray.numpy(), g_xy.numpy()
-        p = ray[::1000, :] + T
-        ax.scatter(p[:, 0], p[:, 1], p[:, 2], c="g", s=5, label="rays")
-        ax.scatter(g_xy[::1000, 0], g_xy[::1000, 1], np.zeros_like(g_xy[::1000, 0]), c="r", s=5, label="intersections")
-        ax.scatter(pos_x, pos_y, pos_z, c="b", s=5, label="origin")
-
-        basis(ax, T, R, length=1)
-
-        field(ax, 400, 400)
-        ax.set_xlabel("x, feet")
-        ax.set_ylabel("y, feet")
-        ax.set_zlabel("z, feet")
-        plt.legend()
-        plt.tight_layout()
-        axis_equal_3d(ax)
-
-        plt.figure("Segmentation Rendering")
-        plt.imshow(img.numpy().reshape((height, width)))
+        plt.figure("Rendered " + title)
+        plt.imshow(img)
         plt.colorbar()
+    else:
+        img = None
 
-    return img
-
-
-def build_vars(calib):
-    print(calib)
-    fov, pos_x, pos_y, pos_z, phi, theta = calib
-
-    fov, phi, theta = tf.Variable([fov], dtype=tf.float32), \
-                      tf.Variable([phi], dtype=tf.float32), \
-                      tf.Variable([theta], dtype=tf.float32)
-
-    pos_x, pos_y, pos_z = tf.Variable([pos_x], dtype=tf.float32), \
-                          tf.Variable([pos_y], dtype=tf.float32), \
-                          tf.Variable([pos_z], dtype=tf.float32)
-
-    return fov, pos_x, pos_y, pos_z, phi, theta
+    return img, values, r_idx
 
 
-def tf_optimize(ref, ground_truth, calib_guess):
-    ref = tf.convert_to_tensor(value=ref)
-    print("ref:", ref.shape)
-
-    ground_truth = tf.reshape(tf.convert_to_tensor(value=ground_truth), (-1,))
-    print("ref:", ground_truth.shape)
+def cam_calib(ref, ground_truth, calib_guess, height=1080, width=1920, mask=None, plot=False):
+    initial_guess = opt_render(ref, *calib_guess, mask=mask, plot=True, title="Initial Guess")
+    # print(ground_truth, "\n\n", initial_guess)
 
     vars = build_vars(calib_guess)
 
+    np_rays, r_idx = prep_rays(height, width, mask, plot=True)
+    tf_rays = tf.constant(tf.convert_to_tensor(value=np_rays, dtype=tf.float32))
+
     def residuals(*vars):
-        current = tf_render(ref, vars)
-        return tf.cast(current - ground_truth, tf.float32)
+        print(vars)
+        if len(vars) != 6:  # tensorflow passed vars wrapped into extra tuple
+            vars = vars[0]
+
+        t0 = time.time()
+        current = trace_rays(tf_rays, vars, ref)
+        print(current)
+        print("Iteration time:", time.time() - t0, "sec")
+
+        return tf.cast(current - ground_truth[1], tf.float32)
 
     # Optimization.
-    _, opt_vars = levenberg_marquardt.minimize(residuals, vars, 10)#, experimental_relax_shapes=True)
+    # _, opt_vars = levenberg_marquardt.minimize(residuals, vars, 3)#, experimental_relax_shapes=True)
 
-    print("opt_vars:", opt_vars)
+    gt = tf.constant(tf.cast(ground_truth[1], tf.float32))
 
-    return opt_vars
+    with tf.GradientTape(persistent=True) as tape:
+        v = trace_rays(tf_rays, vars, ref)
+        # v = vars[0] * 2
+        # print(v)
 
+    print(v)
+    print(vars[0])
+    print(tape.gradient(v, vars[0]))
+    # print(tape.gradient(v, vars[0]).numpy())
 
-# ref_dim and ref_origin in feet (x-right, y-down, top-left image origin)
-#
-def np_render(ref, *calib, ref_dim=(430, 430), ref_origin=(355, 75), plot=False):
-    print(calib)
+    def loss():
+        tf.print("vars:", vars)
 
-    if len(ref.shape) == 1:
-        ref = ref.reshape((ref_dim[0]*60, ref_dim[1]*60))
-    if len(calib) != 6:  # a bug in curve_fit - passed numpy array on last iteration instead of a tuple
-        calib = calib[0].tolist()
+        t0 = time.time()
+        current = tf.cast(trace_rays(tf_rays, vars, ref), tf.float32)
+        print("Iteration time:", time.time() - t0, "sec")
 
-    fov, pos_x, pos_y, pos_z, phi, theta = calib
-    assert(pos_z > 0)  # camera has to be above the ground
-    t0 = time.time()
+        return tf.keras.losses.mean_squared_error(current, gt)
 
-    T = np.array([pos_x, pos_y, pos_z])
-    R = Rotation.from_euler('zx', [phi, theta], degrees=True).as_matrix()
-    # print(T, "\n", R)
+    cost = lambda: tf.keras.losses.mean_squared_error(tf.cast(trace_rays(tf_rays, vars, ref), tf.int32), gt)
 
-    tan_z = np.tan(fov/2 * (np.pi/180))
-    tan_x = tan_z * width / height
-    x, z = np.meshgrid(np.linspace(-tan_x, tan_x, width), np.linspace(tan_z, -tan_z, height))
-    ray = np.stack([x, np.ones((height, width)), z], axis=2).reshape(-1, 3)
-    ray = np.matmul(ray, R)
+    opt = tf.keras.optimizers.SGD(learning_rate=0.3)
+    for i in range(30):
+        step_count = opt.minimize(loss, vars).numpy()
+        print(step_count)
+    opt_vars = vars
 
-    idx_r = np.nonzero(ray[:, 2] < 0)[0]  # only rays pointing downwards can intersect with ground plane
-    g_xy = ray[idx_r, :2] * pos_z / (-ray[idx_r, 2][:, None]) + np.array([pos_x, pos_y])
-
-    ri = ref.shape[0] * (ref_origin[0] - g_xy[:, 1]) / ref_dim[0]
-    ci = ref.shape[1] * (ref_origin[1] + g_xy[:, 0]) / ref_dim[1]
-    idx_rc = np.nonzero((ri >= 0) & (ri < ref.shape[0]) & (ci >= 0) & (ci < ref.shape[1]))[0]
-
-    ri, ci = ri[idx_rc].astype(np.int), ci[idx_rc].astype(np.int)
-    v = np.zeros(g_xy.shape[0])
-    v[idx_rc] = ref[ri, ci]
-
-    img = np.zeros((height * width), dtype=np.uint8)
-    img[idx_r] = v
-    img = img.reshape((height, width))
-    # print("Rendering time:", time.time()-t0, "sec")
+    tf.print("opt_vars:", opt_vars)
 
     if plot:
-        plt.figure("Segmentation Internals", (12, 12))
-        ax = plt.subplot(111, projection='3d', proj_type='ortho')
-        ax.set_title("Segmentation Internals")
-
-        p = ray[::1000, :] + T
-        ax.scatter(p[:, 0], p[:, 1], p[:, 2], c="g", s=5, label="rays")
-        ax.scatter(g_xy[::1000, 0], g_xy[::1000, 1], np.zeros_like(g_xy[::1000, 0]), c="r", s=5, label="intersections")
-        ax.scatter(pos_x, pos_y, pos_z, c="b", s=5, label="origin")
-
-        basis(ax, T, R, length=1)
-
-        field(ax, 400, 400)
-        ax.set_xlabel("x, feet")
-        ax.set_ylabel("y, feet")
-        ax.set_zlabel("z, feet")
-        plt.legend()
-        plt.tight_layout()
-        axis_equal_3d(ax)
-
-        plt.figure("Segmentation Rendering")
-        plt.imshow(img)
+        plt.figure("Overlay Before")
+        plt.imshow(np.abs(ground_truth[0] + initial_guess[0]))
         plt.colorbar()
 
-    return img.ravel()
+        optimal = trace_rays(tf_rays, opt_vars, ref)
 
+        img = np.zeros(height*width)
+        img[r_idx] = optimal.numpy()
+        img = img.reshape((height, width))
 
-def scipy_optimize(ref, ground_truth, calib_guess):
-    # diag = [1, 1/10, 1/10, 1/10, 1/10, 1]
-    # diag = [1, 100, 100, 10, 100, 1]
-    diag = [1, 1, 1, 1, 1, 1]
-
-    # popt, pcov = curve_fit(render, ref.ravel(), ground_truth.ravel(), calib_guess, epsfcn=0.01, factor=100, diag=diag)
-    popt = [4.97631985, 410.22153594, 394.98170901, 49.42902477, 226.06108039, 4.9159036]
-    print("guess:", calib_guess)
-    print("opt:", popt)
-    print("true:", calib_true)
-
-    return popt
+        plt.figure("Overlay After")
+        plt.imshow(np.abs(ground_truth[0] + img))
+        plt.colorbar()
 
 
 if __name__ == "__main__":
     reference_filename = "../baseball_field/baseball_field_segmentation_5px_per_in"  # assumes png
+    ref = load_reference(reference_filename, plot=True)
+    ref = tf.constant(tf.convert_to_tensor(value=ref, dtype=tf.float32))
+    w, h = 1920, 1080
 
-    # cache_reference(reference_filename, plot=True)
+    mask = np.ones((h, w), dtype=np.bool)
+    mask[750:, 1500:] = 0
 
-    ref = load_reference(reference_filename, plot=False)
+    plt.figure("Mask")
+    plt.imshow(mask)
 
     # vertical_fov (deg), position (x', y', z'), view_dir_phi (deg), view_dir_theta (deg)
-    calib_true = (5, 400, 400, 50, 180+45, 5)
+    calib_true = (5, 400, 400, 50, 180 + 45, 5)
+    ground_truth = opt_render(ref, *calib_true, mask=mask, plot=True, title="Ground Truth")
 
-    # test = tf_render(tf.convert_to_tensor(value=ref), *build_vars(calib_true), plot=True).numpy().reshape((height, width))
-
-    ground_truth = np_render(ref, *calib_true, plot=False).reshape((height, width))
-
-    plt.figure("Ground Truth")
-    plt.imshow(ground_truth)
-    plt.colorbar()
-
-    # plt.show()
-    # exit()
-
-    calib_guess = (4.9, 410, 395, 45, 180+46, 4.9)
-    initial_guess = np_render(ref, *calib_guess).reshape((height, width))
-
-    plt.figure("Initial Guess")
-    plt.imshow(initial_guess)
-    plt.colorbar()
-
-    # calib_opt = scipy_optimize(ref, ground_truth, calib_guess)
-    # optimized = np_render(ref, *calib_opt).reshape((height, width))
-
-    calib_opt = tf_optimize(ref, ground_truth, calib_guess)
-    optimized = tf_render(tf.convert_to_tensor(value=ref), *calib_opt).numpy().reshape((height, width))
-
-    plt.figure("Overlay Before")
-    plt.imshow(np.abs(ground_truth + initial_guess))
-    plt.colorbar()
-
-    plt.figure("Overlay After")
-    plt.imshow(np.abs(ground_truth + optimized))
-    plt.colorbar()
+    calib_guess = (4.9, 410, 395, 45, 180 + 46, 4.9)
+    calib_opt = cam_calib(ref, ground_truth, calib_guess, mask=mask, plot=True)
 
     plt.show()
