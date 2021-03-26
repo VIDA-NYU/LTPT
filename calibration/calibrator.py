571c00ea6bfe1a39898b8d7e805ef9eb97e4efb9
ypiadyk <yurii.piadyk@gmail.com>
2021-03-26 06:25:05 -0400

    Improve calibrator

--- calibration/calibrator.py
+++ calibration/calibrator.py
@@ -1,13 +1,9 @@
 import time
-import math
 import numpy as np
 import matplotlib.pyplot as plt
 import tensorflow as tf
-from tensorflow import keras
 from tensorflow_graphics.geometry.transformation import rotation_matrix_3d
 from tensorflow_addons import image as tfa_image
-
-# tf.set_default_graph()
 
 
 def line(ax, p1, p2, *args, **kwargs):
@@ -33,7 +29,7 @@ def field(ax, dim_x, dim_y, length=30, *args, label="field", **kwargs):
 
 def axis_equal_3d(ax):
     extents = np.array([getattr(ax, 'get_{}lim'.format(dim))() for dim in 'xyz'])
-    sz = extents[:,1] - extents[:,0]
+    sz = extents[:, 1] - extents[:, 0]
     centers = np.mean(extents, axis=1)
     maxsize = max(abs(sz))
     r = maxsize/2
@@ -41,18 +37,22 @@ def axis_equal_3d(ax):
         getattr(ax, 'set_{}lim'.format(dim))(ctr - r, ctr + r)
 
 
-class Calibrator:  # (keras.Model):
-    def __init__(self, ref, mask, rate=0.02, defaults=None, scales=None, plot=False, **kwargs):
-        # super(keras.Model, self).__init__(**kwargs)
-        self.optimizer = tf.keras.optimizers.SGD(learning_rate=rate)
+class Calibrator:
+    def __init__(self, ref, mask, defaults=None, scales=None, plot=False):
+        # self.optimizer = tf.keras.optimizers.SGD()
+        self.optimizer = tf.keras.optimizers.Adam()
 
         self.prep_ref(ref, plot=plot)
         self.prep_rays(mask, plot=plot)
 
-        self.build_scaled_vars(defaults or [0] * 6, scales or [1] * 6)
+        self.build_scaled_vars(defaults or [0] * 7, scales or [100, 100, 10, 100, 50, 100, 5])
+        self.reset()
 
     def prep_ref(self, ref, ref_dim=(430, 430), ref_origin=(355, 75), plot=False):
         # assert(ref.shape == (5160, 5160))
+        print("Reference:", ref.shape)
+        ref[[0, -1], :] = 0
+        ref[:, [0, -1]] = 0
 
         self.ref = tf.convert_to_tensor(value=ref, dtype=tf.float32)
         self.ref_shape = tf.constant(ref.shape, dtype=tf.float32)
@@ -60,8 +60,14 @@ class Calibrator:  # (keras.Model):
         self.ref_dim = tf.constant(ref_dim, dtype=tf.float32)
         self.ref_origin = tf.constant(ref_origin, dtype=tf.float32)
 
+        if plot:
+            plt.figure("Reference", (16, 9))
+            plt.imshow(ref)
+            plt.colorbar()
+            plt.tight_layout()
+
     def prep_rays(self, mask, plot=False):
-        print("resolution: ", mask.shape)
+        print("Resolution: ", mask.shape)
         h, w = mask.shape
         x, z = np.meshgrid(np.linspace(-w / h, w / h, w), np.linspace(1, -1, h))
         rays = np.stack([x, np.ones(mask.shape), z], axis=2).reshape(-1, 3)
@@ -91,34 +97,43 @@ class Calibrator:  # (keras.Model):
         self.r_idx = tf.convert_to_tensor(value=r_idx, dtype=tf.int32)
 
     def build_scaled_vars(self, defaults, scales):
-        print("defaults: ", defaults)
-        print("scales: ", scales)
+        print("Defaults: ", defaults)
+        print("Scales: ", scales)
 
+        self.defaults, self.scales = defaults, scales
         self.to_rad = tf.constant(np.pi / 180.)
 
         self.all_scales = [tf.constant(scale, dtype=tf.float32) for scale in scales]
-        self.s_fov, self.s_pos_x, self.s_pos_y, self.s_pos_z, self.s_phi, self.s_theta = self.all_scales
+        self.s_pos_x, self.s_pos_y, self.s_pos_z, self.s_phi, self.s_theta, self.s_dist, self.s_fov = self.all_scales
 
         self.all_vars = [tf.Variable([default / scale], dtype=tf.float32) for default, scale in zip(defaults, scales)]
-        self.fov, self.pos_x, self.pos_y, self.pos_z, self.phi, self.theta = self.all_vars
+        self.pos_x, self.pos_y, self.pos_z, self.phi, self.theta, self.dist, self.fov = self.all_vars
 
-        tf.print("init_vars:", self.all_vars)
+        self.loc_vars = [self.pos_x, self.pos_y, self.pos_z, self.phi, self.theta]
+        self.pos_vars = [self.pos_x, self.pos_y, self.pos_z]
+        self.rot_vars = [self.phi, self.theta]
+        self.cam_vars = [self.theta, self.dist, self.fov]
 
-    # @tf.function
+    def reset(self):
+        self.losses, self.configs = [], []
+
+        for var, default, scale in zip(self.all_vars, self.defaults, self.scales):
+            var.assign([default / scale])
+        tf.print("Default vars:", self.all_vars)
+
     def trace_rays(self, inputs):
-        t0 = tf.timestamp()
-        # t1 = time.time()
-
-        fov, pos_x, pos_y, pos_z, phi, theta = inputs
-
-        fov = (fov + self.fov * self.s_fov) * self.to_rad
-        phi = (phi + self.phi * self.s_phi) * self.to_rad
-        theta = (theta + self.theta * self.s_theta) * self.to_rad
+        pos_x, pos_y, pos_z, phi, theta, dist, fov = inputs
 
         pos_x = pos_x + self.pos_x * self.s_pos_x
         pos_y = pos_y + self.pos_y * self.s_pos_y
         pos_z = pos_z + self.pos_z * self.s_pos_z
+        dist = dist + self.dist * self.s_dist
 
+        phi = (phi + self.phi * self.s_phi) * self.to_rad
+        theta = (theta + self.theta * self.s_theta) * self.to_rad
+        fov = (fov + self.fov * self.s_fov) * self.to_rad
+
+        # angle > 0 means clockwise in rotation_matrix_3d
         rot_z = rotation_matrix_3d.from_axis_angle(tf.constant([0., 0., 1.]), phi)
         rot_x = rotation_matrix_3d.from_axis_angle(tf.constant([1., 0., 0.]), theta)
         rot = tf.matmul(rot_x, rot_z)
@@ -127,12 +142,13 @@ class Calibrator:  # (keras.Model):
         sc = tf.concat([tan_z, tf.constant([1.]), tan_z], axis=0)
         rays = tf.multiply(self.rays, sc[None, :])
         rays = tf.matmul(rays, rot)
-        # clamp rays pointing to the sky
-        rays = tf.minimum(rays, tf.constant([1000., 1000., -0.001])[None, :])
+        rays = tf.minimum(rays, tf.constant([1000., 1000., -0.001])[None, :])  # clamp rays pointing to the sky
 
-        dist = tf.divide(pos_z, -rays[:, 2])
-        xy = tf.multiply(rays[:, :2], dist[:, None])
-        xy = tf.add(xy, tf.concat([pos_x, pos_y], axis=0)[None, :])
+        axis = tf.matmul(tf.constant([0., 1., 0.])[None, :], rot)[0, :]
+        height = pos_z - dist * axis[2]
+        offset = tf.concat([pos_x, pos_y], axis=0) - dist * axis[:2]
+        xy = tf.multiply(rays[:, :2], tf.divide(height, -rays[:, 2])[:, None])
+        xy = tf.add(xy, offset[None, :])
 
         ri = tf.divide(tf.multiply(self.ref_shape[0], tf.subtract(self.ref_origin[0], xy[:, 1])), self.ref_dim[0])
         ci = tf.divide(tf.multiply(self.ref_shape[1], tf.add(self.ref_origin[1], xy[:, 0])), self.ref_dim[1])
@@ -143,8 +159,6 @@ class Calibrator:  # (keras.Model):
         cri = tf.stack([ci, ri], axis=1)
         values = tfa_image.resampler(self.ref[None, :, :, None], cri[None, :, :])
 
-        # print("Ray-Tracing time:", time.time() - t1, "sec")
-        tf.print("Ray-Tracing time:", tf.timestamp() - t0, "sec")
         return values[0, :, 0]
 
     def make_image(self, values, plot=False, title="Image"):
@@ -154,7 +168,7 @@ class Calibrator:  # (keras.Model):
         img = img.reshape(shape)
 
         if plot:
-            plt.figure("Rendered " + title, (16, 9))
+            plt.figure(title, (16, 9))
             plt.imshow(img)
             plt.colorbar()
             plt.tight_layout()
@@ -168,60 +182,97 @@ class Calibrator:  # (keras.Model):
         plt.colorbar()
         plt.tight_layout()
 
+    def plot_loss(self):
+        plt.figure("Loss")
+        plt.gcf().clear()
+        plt.plot(np.arange(len(self.losses)), self.losses)
+        plt.tight_layout()
+
     def __call__(self, inputs):
         return self.trace_rays(inputs)
 
-    def update(self, guess, gt):
-        i = self.optimizer.iterations
-        tf.print("all_vars %d:" % i, self.all_vars)
+    def update(self, guess, gt, vars=None):
+        i, config = self.optimizer.iterations, self.get_config(guess)
+        self.configs.append(config)
+        print("Config %d:" % i, config)
 
         with tf.GradientTape() as tape:
             loss = tf.keras.losses.mean_squared_error(self(guess), gt)
 
-        t0 = tf.timestamp()
-        # t1 = time.time()
-        grads = tape.gradient(loss, self.all_vars)
-        # print("Gradients time:", time.time() - t1, "sec")
-        tf.print("Gradients %d time:" % i, tf.timestamp() - t0, "sec")
+        self.losses.append(loss.numpy())
+        vars = vars or self.all_vars
+        grads = tape.gradient(loss, vars)
 
-        self.optimizer.apply_gradients(zip(grads, self.all_vars))
+        self.optimizer.apply_gradients(zip(grads, vars))
 
-    def optimize(self, guess, gt, epoch=50, rate=0.01, decay=1., plot=False, skip=5):
+    def get_config(self, base):
+        adjustments = [float((var * scale).numpy()) for var, scale in zip(self.all_vars, self.all_scales)]
+        return tuple([b + a for b, a in zip(base, adjustments)])
+
+    def set_config(self, config, base):
+        adjustments = [c - b for c, b in zip (config, base)]
+
+        for var, adj, scale in zip(self.all_vars, adjustments, self.scales):
+            var.assign([adj / scale])
+        tf.print("Chosen vars:", self.all_vars)
+
+    def optimize(self, guess, gt, vars=None, epoch=50, rate=0.01, decay=1., plot=False, skip=5, last=True):
         if plot:
             gt_img = self.make_image(gt, plot=plot, title="Ground Truth")
             guess_img = self.make_image(self(guess), plot=plot, title="Initial Guess")
             self.overlay(gt_img, guess_img, "Before")
 
         print("\nOptimizing...")
+        tf.print("Init vars:", self.all_vars)
         t0 = time.time()
+        vars = vars or self.all_vars
 
         for i in range(epoch):
-            self.optimizer.learning_rate = rate * math.exp(-decay * i / epoch)
+            self.optimizer.learning_rate = rate * np.exp(-decay * i / epoch)
 
             ti = time.time()
-            self.update(guess, gt)
+            self.update(guess, gt, vars=vars)
             time.sleep(0.01)
-            print("Iteration %d:" % i, time.time() - ti, "sec", "\n")
+            # print("Iteration %d:" % i, time.time() - ti, "sec", "\n")
 
             if plot and i % skip == 0:
                 self.overlay(gt_img, self.make_image(self(guess)), "Progress")
-                plt.pause(0.05)
+                plt.pause(0.01)
+                self.plot_loss()
+                plt.pause(0.01)
 
-        tf.print("opt_vars:", self.all_vars)
+        tf.print("Last vars:", self.all_vars)
         print("Optimization time:", time.time() - t0, "sec")
 
-        if plot:
-            opt_img = self.make_image(self(guess), plot=plot, title="Optimized")
-            self.overlay(gt_img, opt_img, "After")
+        best = int(np.argmin(self.losses))
+        self.best_config = self.configs[best]
+        print("Best config %d:" % best, self.best_config)
+        print("Min loss so far:", self.losses[best])
+        self.set_config(self.best_config, guess)
 
-        adjustments = [float((var * scale).numpy()) for var, scale in zip(self.all_vars, self.all_scales)]
-        return [g + a for g, a in zip(guess, adjustments)]
+        if plot and last:
+            self.reset()  # To plot with opt_config instead of guess + vars
+            opt_img = self.make_image(self(self.best_config), plot=plot, title="Last Iteration")
+            self.overlay(gt_img, opt_img, "After")
+            self.plot_loss()
+
+        return self.best_config
+
+    def calibrate(self, guess, gt, **kw):
+        t0 = time.time()
+        self.optimize(guess, gt, vars=self.all_vars, last=False, epoch=50, rate=0.02, decay=2, **kw)
+        self.optimize(guess, gt, vars=self.loc_vars, last=False, epoch=50, rate=0.01, decay=2, **kw)
+        self.optimize(guess, gt, vars=self.rot_vars, last=False, epoch=30, rate=0.01, decay=1, **kw)
+        self.optimize(guess, gt, vars=self.cam_vars, last=False, epoch=20, rate=0.005, decay=1, **kw)
+        self.optimize(guess, gt, vars=self.all_vars, last=True, epoch=20, rate=0.001, decay=1, **kw)
+        print("Total optimization time:", time.time() - t0, "sec")
+        return self.best_config
 
 
 def load_reference(filename, plot=False):
     ref = np.load(filename + ".npy")
     ref = ref[2::5, 2::5]  # make it 1px/in (faster optimization)
-    print("reference:", ref.shape, ref.dtype)
+    # print("reference:", ref.shape, ref.dtype)
 
     if plot:
         plt.figure("Gray Reference", (16, 9))
@@ -244,22 +295,40 @@ def gen_mask(w, h, plot=False):
     return mask
 
 
-if __name__ == "__main__":
+def validate():
     reference_filename = "../baseball_field/baseball_field_segmentation_5px_per_in"
 
     ref = load_reference(reference_filename, plot=False)
     mask = gen_mask(1920, 1080, plot=False)
 
-    cal = Calibrator(ref, mask, scales=[1, 100, 100, 10, 10, 1], plot=False)
-    # cal.build(tf.TensorShape([6]))
-    # cal.compile(loss="mse", optimizer="nadam")
+    cal = Calibrator(ref, mask, plot=True)
 
-    calib_true = (5, 400, 400, 50, 180 + 45, 5)
-    calib_guess = (4.9, 410, 395, 45, 180 + 46, 4.9)
-    # calib_guess = (4.5, 450, 390, 65, 180 + 47, 4.9)
+    # Behind Pitcher
+    calib_true = (45, 45, 5, 180 + 45, 5, 400, 5)
+    calib_guess = (55, 50, 7, 180 + 46, 6, 350, 4.5)
 
-    calib_opt = cal.optimize(calib_guess, cal(calib_true), epoch=30, rate=0.02, decay=1.5, plot=True, skip=5)
+    # Pitcher Side (Right Handed)
+    # calib_true = (45, 45, 10, 90 + 55, 7, 200, 10)
+    # calib_guess = (75, 30, 6, 90 + 65, 6, 250, 8)
 
-    print("Optimal calibration:", calib_opt)
+    # Batter Side (Right Handed)
+    # calib_true = (0, 0, 5, -55, 4, 100, 15)
+    # calib_guess = (5, 10, 3, -40, 3, 122, 12)
+
+    # Behind Batter
+    # calib_true = (40, 40, 0, 45, 20, 300, 25)
+    # calib_guess = (50, 60, 10, 50, 15, 330, 22)
+
+    calib_opt = cal.calibrate(calib_guess, cal(calib_true), plot=True)
+    print("\nOptimal calibration:", calib_opt)
+
+
+def test():
+    pass
+
+
+if __name__ == "__main__":
+    validate()
+    # test()
 
     plt.show()
